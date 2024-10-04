d00e80c4fb6b8b6b71ae13d779389841c9105b78
TeddyLin <i100308326@gmail.com>
2024-10-04 15:30:07 +0800

    feat: wallet icon

--- src/pages/layout/footer.tsx
+++ src/pages/layout/footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
 
   return (
     <div className="px-2 h-[80px] flex gap-4 bg-bg-dark">
-      <div className="py-6 w-1/3" onClick={() => openWallet()}>
+      <div className="py-6 w-1/3" onClick={() => openWallet({ topMenuType: 'close' })}>
         <WalletIcon className="mx-auto w-10 h-10" />
       </div>
       <div className="py-6 w-1/3" onClick={() => navigate('/')}>
