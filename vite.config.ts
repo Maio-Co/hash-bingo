fde3f1a1f16961e05a665437e647a99aa0208b77
TeddyLin <i100308326@gmail.com>
2024-10-06 18:34:42 +0800

    feat:PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     // base: '/hash-bingo/',
     plugins: [svgr(), react()],
